6424c6ce8036852ee4e942ea92b5723b8119a532
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-05-05 12:09:42 +0530

    extract text bug solved

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
-document.getElementById("summarize").addEventListener("click", async () => {
-  console.log("Summarize button clicked");
+document.getElementById("summarize").addEventListener("click", () => {
+  const result = document.getElementById("result");
+  result.textContent = "Extracting Text...";
+
+  chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+    chrome.tabs.sendMessage(
+      tab.id,
+      { type: "GET_ARTICLE_TEXT" },
+      ({ text }) => {
+        result.textContent = text
+          ? text.slice(0, 300) + "..."
+          : "No text found";
+      }
+    );
+  });
 });
